749f30a3a54a5761edb6bf83634a0553e4ca0b82
nepatil2 <nehapatil22899@gmail.com>
2021-12-15 18:19:17 +0530

    Appointment Details

---
+++ Appointment.java
@@ -0,0 +1,17 @@
+package com.bl.hms;
+
+import java.util.Date;
+
+public class Appointment {
+
+    String patientId;
+    String doctorName;
+    Date appointmentDate;
+
+}
+
+//project -> provides-> MVC
+//mvc  == model view controller
+//model == anything related to the data
+//view == user interface layout - GUI, GLI
+//controller == contolls the model and view business logics are written in controller
