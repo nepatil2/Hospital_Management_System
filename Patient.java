8da4c382fd988a0434f81263a7a0dc96115cbb9e
nepatil2 <nehapatil22899@gmail.com>
2021-12-15 18:11:30 +0530

    Patient Details

---
+++ Patient.java
@@ -0,0 +1,17 @@
+package com.bl.hms;
+
+public class Patient {
+
+    enum Gender{
+        MALE, FEMALE, GENDER;
+    }
+
+    String name;
+    int age;
+    long mobileNumber;
+    String patienId;
+    String emailId;
+    String address;
+    String disease;
+
+}
