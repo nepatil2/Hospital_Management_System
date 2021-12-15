4aeadaa5ea14cc3abd11892287d96f56245906da
nepatil2 <nehapatil22899@gmail.com>
2021-12-15 18:13:50 +0530

    Patient Repo

---
+++ PatientRepo.java
@@ -0,0 +1,11 @@
+package com.bl.hms;
+
+import java.util.ArrayList;
+import java.util.List;
+public class PatientRepo {
+    List patientList = new ArrayList();
+
+    public void add(Patient patient){
+        patientList.add(patient);
+    }
+}
