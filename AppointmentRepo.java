a3042537dcbd3d7d7b9d3f7cf0704eb18b1badf9
nepatil2 <nehapatil22899@gmail.com>
2021-12-15 18:21:06 +0530

    Appointment Repo

---
+++ AppointmentRepo.java
@@ -0,0 +1,13 @@
+package com.bl.hms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AppointmentRepo {
+    List appointmentList = new ArrayList();
+
+    public  void add(Appointment appointment){
+        appointmentList.add(appointment);
+
+    }
+}
